988955b5079a0786e3e71f584b169e67f225ef8c
KMITL-Jer <69011514@kmitl.ac.th>
2026-07-17 06:26:26 +0000

    Quiz1_week3_V2 git commit -m quiz1_week3_v2 git push

--- quiz1/exercise_quiz1.c
+++ quiz1/exercise_quiz1.c
@@ -2,11 +2,11 @@
 
 int main(){
 
-    printf("Programming: A\n"); 
-    printf("Drawing    : B+\n");
-    printf("Mechanics  : B+\n");
-    printf("Math       : B\n");
-    printf("GPS        : %.2f\n", 3.5);
+    printf("Programming : A\n"); 
+    printf("Drawing     : B+\n");
+    printf("Mechanics   : B+\n");
+    printf("Math        : B\n");
+    printf("GPS         : %.2f\n", 3.5);
 
     return 0;
 }
